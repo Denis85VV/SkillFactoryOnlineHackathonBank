eabf3ab26dec66f6e3e54dd8cc028156bc323992
Denis Vv <denisvv@Ruslans-MacBook-Pro.local>
2021-04-05 19:28:32 +0300

    add project

---
+++ Bank/BalanceVC.swift
@@ -0,0 +1,29 @@
+//
+//  BalanceVC.swift
+//  Bank
+//
+//  Created by Denis Vv on 03.04.2021.
+//
+
+import UIKit
+
+class BalanceVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Bank/BankModel.swift
@@ -0,0 +1,8 @@
+//
+//  BankModel.swift
+//  Bank
+//
+//  Created by Denis Vv on 04.04.2021.
+//
+
+import Foundation
